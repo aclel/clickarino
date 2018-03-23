c28af675b7d13a45d3476efb6f6b8840a79ff266
Andrew Cleand <andrew.cleland3@gmail.com>
2018-03-23 18:28:35 +1000

    Initial commit

---
+++ client/index.js
@@ -0,0 +1,164 @@
+var uploadsBucketName = 'clickarino-uploads';
+var bucketRegion = 'ap-southeast-2';
+var IdentityPoolId = 'ap-southeast-2:95798f07-66e2-4b68-9b13-9d3a4f985dfd';
+var iotHost = 'ak4y3bhc8ktbp.iot.ap-southeast-2.amazonaws.com';
+
+var AWSIoTData = require('aws-iot-device-sdk');
+
+AWS.config.update({
+    region: bucketRegion,
+    credentials: new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: IdentityPoolId
+    })
+});
+
+var clientId = 'clickarino-' + (Math.floor((Math.random() * 100000) + 1));
+
+// Create the AWS IoT device object.  Note that the credentials must be 
+// initialized with empty strings; when we successfully authenticate to
+// the Cognito Identity Pool, the credentials will be dynamically updated.
+const mqttClient = AWSIoTData.device({
+    
+       // Set the AWS region we will operate in.
+       region: AWS.config.region,
+    
+       // Set the AWS IoT Host Endpoint
+       host: iotHost,
+    
+       // Use the clientId created earlier.
+       clientId: clientId,
+    
+       // Connect via secure WebSocket
+       protocol: 'wss',
+       //
+       // Set the maximum reconnect time to 8 seconds; this is a browser application
+       // so we don't want to leave the user waiting too long for reconnection after
+       // re-connecting to the network/re-opening their laptop/etc...
+       maximumReconnectTimeMs: 8000,
+    
+       // Enable console debugging information (optional)
+       debug: false,
+    
+       // IMPORTANT: the AWS access key ID, secret key, and sesion token must be 
+       // initialized with empty strings.
+       accessKeyId: '',
+       secretKey: '',
+       sessionToken: ''
+});
+
+
+var cognitoIdentity = new AWS.CognitoIdentity();
+AWS.config.credentials.get(function(err, data) {
+   if (!err) {
+        console.log('Retrieved identity: ' + AWS.config.credentials.identityId);
+        var params = {
+            IdentityId: AWS.config.credentials.identityId
+        };
+        cognitoIdentity.getCredentialsForIdentity(params, function(err, data) {
+            if (!err) {
+                // Update our latest AWS credentials; the MQTT client will use these
+                // during its next reconnect attempt.
+                mqttClient.updateWebSocketCredentials(
+                    data.Credentials.AccessKeyId,
+                    data.Credentials.SecretKey,
+                    data.Credentials.SessionToken
+                );
+            } else {
+                console.log('Error retrieving credentials: ' + err);
+            }
+        });
+    } else {
+        console.log('Error retrieving identity:' + err);
+    }
+});
+
+window.saveByteArray = function (data, name) {
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+
+    var blob = new Blob(data, {type: "octet/stream"}),
+        url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = name;
+    a.click();
+    window.URL.revokeObjectURL(url);
+};
+
+// Connect handler
+window.mqttClientConnectHandler = function() {
+    console.log('connect');
+    mqttClient.subscribe('ap-southeast-2:627265be-4e99-4292-9b25-2e96f08af0db');
+ };
+ 
+ // Reconnect handler
+ window.mqttClientReconnectHandler = function() {
+    //console.log('reconnect');
+ };
+
+window.onStartedDownload = function(id) {
+    console.log(`Started downloading: ${id}`);
+}
+  
+window.onFailed = function(id) {
+    console.log(`Download failed: ${error}`);
+}
+
+// Message handler
+ window.mqttClientMessageHandler = function(topic, pload) {
+    var payload = JSON.parse(pload);
+    console.log(payload);
+    if (payload.hasOwnProperty('Bucket') && payload.hasOwnProperty('Key')) {
+        console.log("downloading click");
+        var params = { Bucket: payload.Bucket, Key: payload.Key}
+        s3.getObject(params, function(err, data) {
+            if (err) {
+                console.log(err, err.stack);
+            } else {
+                saveByteArray(data.Body, "click.wav");
+            }
+        });
+    } else if (payload.hasOwnProperty('Message')) {
+        var messageList = document.getElementById("messages");
+        var messageElement = document.createElement("li");
+        var textNode = document.createTextNode(payload.Message);
+        messageElement.appendChild(textNode);
+        messageList.appendChild(messageElement);
+    } else {
+        console.log(payload);
+    }
+ };
+
+mqttClient.on('connect', window.mqttClientConnectHandler);
+mqttClient.on('reconnect', window.mqttClientReconnectHandler);
+mqttClient.on('message', window.mqttClientMessageHandler);
+
+var s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    params: {Bucket: uploadsBucketName}
+});
+
+window.uploadTrack = function() {
+    var identityId = AWS.config.credentials.identityId;
+
+    var files = document.getElementById('file').files;
+    if (!files.length) {
+      return alert('Please choose a file to upload first.');
+    }
+    console.log("uploading track");
+    var file = files[0];
+    var fileName = file.name;
+  
+    s3.upload({
+        Key: fileName,
+        Body: file,
+        Metadata: {
+            "IdentityId": identityId
+        }
+    }, function(err, data) {
+        if (err) {
+            return alert('There was an error uploading your track: ', err.message);
+        }
+        alert('Successfully uploaded your track.');
+    });
+}
---
+++ lambda/create_click.py
@@ -0,0 +1,132 @@
+''' This is a lambda function to create a click track
+    when an audio file is uploaded to S3.
+
+    1. Download the file from the clickarino-uploads bucket
+    2. Get the cognito id from the S3 object metadata and connect to the topic
+    3. Create the click track
+    4. Upload the file to the clickarino-clicktracks bucket
+    5. Send a message back to the client (via IoT) with the key for the file
+'''
+try:
+  import unzip_requirements
+except ImportError:
+  pass
+
+import os
+import json
+import shutil
+import boto3
+import librosa
+import librosa.output
+import mir_eval
+import numpy as np
+import urllib.parse
+
+LAMBDA_TASK_ROOT = os.environ.get('LAMBDA_TASK_ROOT', os.path.dirname(os.path.abspath(__file__)))
+
+# binaries are stored in ./bin
+CURR_BIN_DIR = os.path.join(LAMBDA_TASK_ROOT, 'bin')
+
+# binaries on lambda can only be executed from /tmp/bin
+BIN_DIR = '/tmp/bin'
+os.environ['PATH'] += ':' + BIN_DIR
+
+CLICKTRACK_BUCKET_NAME = 'clickarino-clicktracks'
+
+s3_client = boto3.client('s3')
+iot_client = boto3.client('iot-data')
+
+
+def main(event, context):
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+
+        metadata = s3_client.head_object(Bucket=bucket, Key=key)['Metadata']
+        cognito_identity_id = metadata['identityid']
+
+        payload = { 'Message': 'Firing up the clickarino' }
+        response = iot_client.publish(
+            topic=cognito_identity_id,
+            qos=1,
+            payload=json.dumps(payload)
+        )
+
+        filepath = os.path.join('/tmp', os.path.basename(key))
+        s3_client.download_file(bucket, key, filepath)
+
+        payload = { 'Message': 'Clickifying the file...' }
+        response = iot_client.publish(
+            topic=cognito_identity_id,
+            qos=1,
+            payload=json.dumps(payload)
+        )
+
+        output_path = create_click(filepath)
+        # s3_client.upload_file(output_path, CLICKTRACK_BUCKET_NAME, 'click.wav')
+
+        tags = {
+            'IdentityId': cognito_identity_id
+        }
+        tag_set = urllib.parse.urlencode(tags)
+
+        s3_client.put_object(
+            Bucket=CLICKTRACK_BUCKET_NAME,
+            Key='click.wav',
+            Tagging=tag_set
+        )
+
+        payload = { 'Message': 'Clickification complete - sending the file back to you now'}
+        response = iot_client.publish(
+            topic=cognito_identity_id,
+            qos=1,
+            payload=json.dumps(payload)
+        )
+
+        payload = {
+            'Bucket': CLICKTRACK_BUCKET_NAME,
+            'Key': 'click.wav'
+        }
+        response = iot_client.publish(
+            topic=cognito_identity_id,
+            qos=1,
+            payload=json.dumps(payload)
+        )
+
+
+def create_click(filepath):
+    _init_bin('ffmpeg')
+
+    # Load the file
+    filename = os.path.basename(filepath)
+    file_extension = filename.split('.')[1]
+    y, sr = librosa.load(filepath)
+
+    # Use the onset envelope to get the tempo and track the beats
+    onset_env = librosa.onset.onset_strength(y, sr=sr, aggregate=np.median)
+    tempo, beats = librosa.beat.beat_track(onset_envelope=onset_env, sr=sr)
+
+    # Get the times of the beats
+    times = librosa.frames_to_time(beats)
+
+    # Create a sound clip with a click at the beat times
+    y_click = mir_eval.sonify.clicks(times, sr, length=len(y))
+
+    # Write a wav file with the click overlayed on top of the track
+    output_path = '/tmp/click.wav'
+    librosa.output.write_wav(output_path, y + y_click, sr)
+
+    return output_path
+
+
+def _init_bin(executable_name):
+    if not os.path.exists(BIN_DIR):
+        os.makedirs(BIN_DIR)
+
+    print('Moving binary {0} to {1}'.format(executable_name, BIN_DIR))
+    binary_path = os.path.join(CURR_BIN_DIR, executable_name)
+    new_path  = os.path.join(BIN_DIR, executable_name)
+    shutil.copy2(binary_path, new_path)
+
+    print('Giving {0} execution permissions'.format(executable_name))
+    os.chmod(new_path, 0o775)
