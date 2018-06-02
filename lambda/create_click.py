de8478a655ff4dc6c348a981ea35df3c37bd4f11
Andrew Cleand <andrew.cleland3@gmail.com>
2018-06-02 22:49:59 +1000

    Update lambda function to use mobilehub bucket

--- lambda/create_click.py
+++ lambda/create_click.py
@@ -31,7 +31,7 @@ CURR_BIN_DIR = os.path.join(LAMBDA_TASK_ROOT, 'bin')
 BIN_DIR = '/tmp/bin'
 os.environ['PATH'] += ':' + BIN_DIR
 
-CLICKTRACK_BUCKET_NAME = 'clickarino-clicktracks'
+CLICKTRACK_BUCKET_NAME = 'clickarino-userfiles-mobilehub-2127974803'
 
 s3_client = boto3.client('s3')
 iot_client = boto3.client('iot-data')
@@ -42,59 +42,59 @@ def main(event, context):
         bucket = record['s3']['bucket']['name']
         key = record['s3']['object']['key']
 
-        metadata = s3_client.head_object(Bucket=bucket, Key=key)['Metadata']
-        cognito_identity_id = metadata['identityid']
+        # metadata = s3_client.head_object(Bucket=bucket, Key=key)['Metadata']
+        # cognito_identity_id = metadata['identityid']
 
-        payload = { 'Message': 'Firing up the clickarino' }
-        response = iot_client.publish(
-            topic=cognito_identity_id,
-            qos=1,
-            payload=json.dumps(payload)
-        )
+        # payload = { 'Message': 'Firing up the clickarino' }
+        # response = iot_client.publish(
+        #     topic=cognito_identity_id,
+        #     qos=1,
+        #     payload=json.dumps(payload)
+        # )
 
         filepath = os.path.join('/tmp', os.path.basename(key))
         s3_client.download_file(bucket, key, filepath)
 
-        payload = { 'Message': 'Clickifying the file...' }
-        response = iot_client.publish(
-            topic=cognito_identity_id,
-            qos=1,
-            payload=json.dumps(payload)
-        )
+        # payload = { 'Message': 'Clickifying the file...' }
+        # response = iot_client.publish(
+        #     topic=cognito_identity_id,
+        #     qos=1,
+        #     payload=json.dumps(payload)
+        # )
 
         output_path = _create_click(filepath)
 
-        tags = {
-            'IdentityId': cognito_identity_id
-        }
-        tag_set = urllib.parse.urlencode(tags)
+        # tags = {
+        #     'IdentityId': cognito_identity_id
+        # }
+        # tag_set = urllib.parse.urlencode(tags)
 
         s3_client.put_object(
             Bucket=CLICKTRACK_BUCKET_NAME,
             Body=open(output_path, 'rb'),
-            Key='click.wav',
+            Key='public/click.wav',
             Metadata={
                 'Content-Disposition': 'attachment; filename=click.wav;'
             },
-            Tagging=tag_set
+            # Tagging=tag_set
         )
 
-        payload = { 'Message': 'Clickification complete - sending the file back to you now'}
-        response = iot_client.publish(
-            topic=cognito_identity_id,
-            qos=1,
-            payload=json.dumps(payload)
-        )
+        # payload = { 'Message': 'Clickification complete - sending the file back to you now'}
+        # response = iot_client.publish(
+        #     topic=cognito_identity_id,
+        #     qos=1,
+        #     payload=json.dumps(payload)
+        # )
 
-        payload = {
-            'Bucket': CLICKTRACK_BUCKET_NAME,
-            'Key': 'click.wav'
-        }
-        response = iot_client.publish(
-            topic=cognito_identity_id,
-            qos=1,
-            payload=json.dumps(payload)
-        )
+        # payload = {
+        #     'Bucket': CLICKTRACK_BUCKET_NAME,
+        #     'Key': 'click.wav'
+        # }
+        # response = iot_client.publish(
+        #     topic=cognito_identity_id,
+        #     qos=1,
+        #     payload=json.dumps(payload)
+        # )
 
 
 def _create_click(filepath):
